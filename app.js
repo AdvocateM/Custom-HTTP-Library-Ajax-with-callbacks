0cb62d0634b0f385579b2bca8727259b5094380a
AdvocateM <tboymaroga7@gmail.com>
2022-10-05 11:13:18 +0200

    Update

--- app.js
+++ app.js
@@ -1,16 +1,53 @@
-// // const http = new easyHTTP;
-// const http = new easyHTTP;
-
-// // get posts 
-// http.get('https://jsonplaceholder.typicode.com/posts');
-
 const http = new easyHTTP;
 
+// // Get Posts
+// http.get('https://jsonplaceholder.typicode.com/posts', function (error, posts) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+// Get Single Post
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (error, post) {
+// if (error) {
+//   console.log(error);
+// } else {
+//   console.log(post);
+// }
+// });
+
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+};
+
+
+// Create Post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, (error, post) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(post);
+//   }
+// })
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function (error, post) {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log(post)
+//   }
+// });
+
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function (error, posts) {
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function (error, respond) {
   if (error) {
-    console.log(err);
+    console.log(error);
   } else {
-    console.log(posts);
+    console.log(respond);
   }
 });
--- easyhttp.js
+++ easyhttp.js
@@ -21,8 +21,47 @@ easyHTTP.prototype.get = function (url, callback) {
 }
 
 
+// Make an HTTP POST Request
+easyHTTP.prototype.post = function (url, data, callback) {
+  this.http.open('POST', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  this.http.onload = () => {
+    callback(null, this.http.responseText);
+  }
+  this.http.send(JSON.stringify(data));
+}
+
+
+// Make an HTTP PUT Request
+easyHTTP.prototype.put = function (url, data, callback) {
+  this.http.open('PUT', url, true);
+  this.http.setRequestHeader('Content-type', 'application/json');
+
+  this.http.onload = () => {
+    callback(null, this.http.responseText);
+  }
+  this.http.send(JSON.stringify(data));
+}
+
+
 // Make an HTTP GET Request
-// Make an HTTP GET Request
-// Make an HTTP GET Request
+easyHTTP.prototype.delete = function (url, callback) {
+  this.http.open('DELETE', url, true);
+
+  // set var for standard function and replace this from he function
+  // let self = this;
+
+  this.http.onload = () => {
+    if (this.http.status === 200) {
+      callback(null, 'Post Deleted');
+    } else {
+      callback(`Error: ${this.http.status}`)
+    }
+  }
+  this.http.send();
+}
+
+
 // Make an HTTP GET Request
 // Make an HTTP GET Request
