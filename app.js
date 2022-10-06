2171f28f1276e03ddad3a9c47e7c9fdbf6954fdd
mr_maroga <45645052+AdvocateM@users.noreply.github.com>
2022-10-06 04:18:41 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
-// // const http = new easyHTTP;
-// const http = new easyHTTP;
+const http = new EasyHTTP;
 
-// // get posts 
-// http.get('https://jsonplaceholder.typicode.com/posts');
+// Get Users
+// http.get('https://jsonplaceholder.typicode.com/users')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
 
-const http = new easyHTTP;
+// User Data
+const data = {
+  name: 'John Doe',
+  username: 'johndoe',
+  email: 'jdoe@gmail.com'
+}
 
-// Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function (error, posts) {
-  if (error) {
-    console.log(err);
-  } else {
-    console.log(posts);
-  }
-});
+// Create User
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// Delete User
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+.then(data => console.log(data))
+.catch(err => console.log(err));
